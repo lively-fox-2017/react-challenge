8c7c1bb755f40fcd6ee6e32425f700dd74bbb07c
JainalArifin <zainalunpam05@gmail.com>
2017-11-14 15:04:05 +0700

    berhasil menampilkan api

--- api-start-war/src/App.js
+++ api-start-war/src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import axios from 'axios'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+import Navbar from './components/Navbar'
+
+export default class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      statrWar: []
+    }
   }
-}
 
-export default App;
+  componentWillMount () {
+    axios.get('https://swapi.co/api/people/1/')
+    .then(({ data }) => {
+      console.log('ini ', data)
+      this.setState({
+        statrWar: data
+      })
+      console.log('state', this.state.statrWar);
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <Navbar/>
+        <ul>
+          <li>{this.state.statrWar.name}</li>
+        </ul>
+      </div>
+    )
+  }
+
+}
