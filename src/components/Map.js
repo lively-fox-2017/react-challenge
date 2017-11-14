3bcbea2986fc28428a06c80bcab8974076c55e0b
sisinduku <saptanto.sindu@gmail.com>
2017-11-14 21:11:05 +0700

    Add Routing

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
-import InfoBox from 'react-google-maps/lib/components/addons/InfoBox';
 import { compose, withProps } from 'recompose'
 
 const Map = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAPSECRET}&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `300px` }} />,
     mapElement: <div style={{ height: `100%` }} />
--- src/components/StopList.js
+++ src/components/StopList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
-import './assets/css/bootstrap.min.css'
+import '../assets/css/bootstrap.min.css'
 
 class StopList extends Component {
   listGroup () {
@@ -20,8 +21,8 @@ class StopList extends Component {
     return (
       <div className="card border-info">
         <div className="card-body">
-          <h5 className="card-title">{this.props.stop.Name}</h5>
-          <h6 className="card-subtitle text-muted">{this.props.stop.StopTooltip.DistanceText}</h6>
+          <Link to={{pathname: `/detail/${this.props.stop.Id}`}}><h5 className="card-title">{this.props.stop.Name}</h5></Link>
+          <h6 className="card-subtitle text-muted">Jarak: {this.props.stop.StopTooltip.DistanceText}</h6>
           <blockquote className="card-blockquote">
             <h6>Angkutan: </h6>
             {this.listGroup()}
