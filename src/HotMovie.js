fa4ff77012645ec3cc912f87d3a77cf56115290a
NNcrawler <rasyid.xyz@gmail.com>
2017-11-14 18:52:42 +0700

    add home screen

--- src/HotMovie.js
+++ src/HotMovie.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class HotMovie extends Component {
   render() {
     return (
-      <div>
-        hot movie
+      <div className="row">
+        <div className="col-md-5 offset-md-1 center">
+          <img className="" src="http://www.giladiskon.com/sites/default/files/photos/photo%20-%201440893415.jpg"/>
+        </div>
       </div>
     )
   }
