ca890821030270403b4ad87a5818b19637c9b086
redhaputra <putra.redha@gmail.com>
2017-11-16 14:07:21 +0700

    add redux-devtools yg keren

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import articles from '../reducers'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const middleware = applyMiddleware(thunk, logger)
+const enhancer = devtools(
+  middleware
+)
 
-const store = createStore(articles, middleware)
+const store = createStore(articles, enhancer)
 
 export default store
