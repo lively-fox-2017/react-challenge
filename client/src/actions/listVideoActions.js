2477a70042cd5f10216cd3f18fb39b4cf29b866b
munawirali <munawirali@gmail.com>
2017-11-16 07:22:10 +0700

    add react-redux

---
+++ client/src/actions/listVideoActions.js
@@ -0,0 +1,18 @@
+export const fetchAllVideo = (listVideo) => {
+  return {
+    type: 'fetchAll',
+    payload: {
+      listVideo
+    }
+  }
+}
+
+export const paramChange = (param) => {
+  console.log('xxxxxx', param);
+  return {
+    type: 'param_change',
+    payload: {
+      param
+    }
+  }
+}
--- client/src/components/Main-before-redux.js
+++ client/src/components/Main-before-redux.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Axios from 'axios'
 import ListVideo from './ListVideo'
 import { connect } from 'react-redux'
-import { fetchAllVideo, paramChange } from '../actions/listVideoActions'
+import { fetchAllVideo } from '../actions/listVideoActions'
 
 class Main extends Component {
   constructor(props) {
@@ -19,9 +19,8 @@ class Main extends Component {
     // https://www.googleapis.com/youtube/v3/search
     .then(response=>{
       // console.log(response);
-      // this.setState({listVideo:response.data.items})
-      this.props.fetchAllVideo(response.data.items)
-      // console.log('adfasds', response.data.items);
+      this.setState({listVideo:response.data.items})
+      console.log('adfasds', response.data.items);
     })
     .catch(err=>{
       console.log(err);
@@ -30,18 +29,17 @@ class Main extends Component {
   componentDidMount(){
     Axios.get('https://www.googleapis.com/youtube/v3/videos?chart=mostPopular&key=AIzaSyD36XAU2xCZCinLW7GnkoLSgmelqNV4_Dg&part=snippet&maxResults=8')
     .then(response=>{
-      // console.log('ini props', this.props);
-      this.props.fetchAllVideo(response.data.items)
-      // console.log('------props.fetchAllVideo', this.props.fetchAllVideo);
+      // console.log(response);
+      // this.setState({listVideo:response.data.items})
+      this.props.fetchAllVideo(response)
+      // console.log('adfasds', response.data.items); response.data.items[0].snippet.thumbnails.medium);
     })
     .catch(err=>{
       console.log(err);
     })
   }
   handleChange(event){
-    console.log('iniprops', this.props);
-    this.props.paramChange(event.target.value);
-    console.log('ini props param', this.props.param);
+    this.setState({param:event.target.value});
     event.preventDefault();
   }
   render(){
@@ -55,7 +53,7 @@ class Main extends Component {
             <div className="input-group">
                     <input onChange={this.handleChange} type="text" className="form-control" placeholder="Search&hellip;"/>
                     <span className="input-group-btn">
-                        <button onClick={()=>this.searchVideo(this.props.param)} type="button" className="btn btn-default">Search</button>
+                        <button onClick={()=>this.searchVideo(this.state.param)} type="button" className="btn btn-default">Search</button>
                     </span>
                 </div>
           </div>
@@ -70,18 +68,14 @@ class Main extends Component {
   }
 }
 const mapStateToProps = (state) => {
-  console.log('--------mapStateToProps', state);
   return {
-    listVideo: state.listVideoReducer.listVideo,
-    param: state.listVideoReducer.param
+    listVideo: state.listVideoReducer.listVideo
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  // console.log('...........dispatch', dispatch);
   return {
-    fetchAllVideo: (listVideo) => dispatch(fetchAllVideo(listVideo)),
-    paramChange: (param) => dispatch(paramChange(param))
+    fetchAllVideo: (listVideo) => dispatch(fetchAllVideo(listVideo))
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Main)
